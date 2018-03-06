a65c42dbd9e1005e41f3cea17910f4f7ccf9456d
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 09:58:09 -0600

    feat: fulfill corresponding test

--- src/components/DogSearchForm.js
+++ src/components/DogSearchForm.js
@@ -3,6 +3,8 @@ import React from 'react'
 function DogSearchForm() {
     return (
         <form className="dog-search-form">
+            <input type="text" className="dog-search-input" />
+            <button type="submit" className="dog-search-button">Search</button>
         </form>
     );
 }
