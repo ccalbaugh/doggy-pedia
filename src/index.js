7e3e3d49235981add87f326174bc80b1cbef7a80
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 12:15:05 -0600

    chore: create redux store and Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DogApp from './components/DogApp'
-import './index.css'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import DogApp from './components/DogApp';
+import './index.css';
 
-ReactDOM.render(<DogApp />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <DogApp />
+    </Provider>, 
+    document.getElementById('root')
+);
