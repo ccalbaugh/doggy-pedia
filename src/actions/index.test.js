aa349d80f24be696a6af73ee4046c2aec06e3f43
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 14:02:02 -0600

    test: ensure there is an action to fetch all breeds

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,13 +1,38 @@
-import { expect } from 'code'
-import * as actions from './index'
-import * as types from './actionTypes'
+import 'isomorphic-fetch';
+import { expect } from 'code';
+import sinon from 'sinon';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import * as actions from './index';
+import * as types from './actionTypes';
 
-it('creates an action to fetch all breeds', () => {
+const createMockStore = configureMockStore([ thunk ]);
+const store = createMockStore({ allBreeds: {} });
+const mockResponse = { message: [ "affenpinscher", "african", "airedale", "akita" ] };
+const json = sinon.stub().returns(mockResponse);
+const fetchStub = sinon.stub(global, 'fetch').resolves({ json });
+
+
+it('creates an action to search for a breed', () => {
 
     const mockBreed = 'boxer';
 
     const expectedAction = { type: types.SEARCH_BREED, breed: mockBreed };
 
     expect(actions.searchBreed(mockBreed)).to.equal(expectedAction);
+
+});
+
+it('creates an async action to fetch all breeds', () => {
+
+    const expectedActionS = [{ type: types.FETCH_ALL_BREEDS, breeds: mockResponse }];
+
+    return store.dispatch(actions.fetchAllBreeds())
+
+    .then(() => {
+
+        expect(store.getActions()).to.equal(expectedActions);
+
+    });
 
 });
