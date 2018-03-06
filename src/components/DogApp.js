55e4153fb8591353e83836139b78e0da8e42a5ca
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 08:36:22 -0600

    feat: create DogApp component to test requirements:

---
+++ src/components/DogApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DogSearchForm from './DogSearchForm'
+
+function App() {
+    return (
+        <main className="DogApp">
+            <DogSearchForm />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/components/DogApp.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { expect } from 'code'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
+import App from './App'
+
+describe('Give `App`', () => {
+
+    let component
+
+    function requiredProps(overrides = {}) {
+        return {
+            ...overrides
+        };
+    }
+
+    function renderComponent(props = requiredProps()) {
+        return shallow(<App {...props} />);
+    }
+
+    beforeEach(() => {
+        component = renderComponent();
+    })
+
+    it('should exist as a `main` with a specific class name', () => {
+
+        expect(component.find('.dog-app').type()).to.equal('main');
+
+    });
+
+    it('should contain a `DogSearchForm', () => {
+
+        expect(component.find('DogSearchForm').exists()).to.be.true()
+
+    });
+
+});
