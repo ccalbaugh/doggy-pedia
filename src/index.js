bb6501bc1de97b2daf6ef65872309f4845a4abe8
Caden Albaugh <cadenalbaugh@gmail.com>
2018-03-06 12:51:00 -0600

    fix: setup with redux
    * feat: add actionTypes file to compile all action types
    
    * test: ensure there is a searchBreed action
    
    * feat: fulfill corresponding test
    
    * test: ensure initial state is returned if none is given
    
    * feat: fulfill corresponding test
    
    * chore: add redux dependencies
    
    * chore: create redux store and Provider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import DogApp from './components/DogApp';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <DogApp />
+    </Provider>, 
+    document.getElementById('root')
+);
