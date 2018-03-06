ceb2168ef99bcbac28d08af75d7fb2cba054b5ed
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 09:50:30 -0600

    test: ensure DogWiki exists as a section

---
+++ src/components/DogWiki.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { expect } from 'code'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
+import DogWiki from './DogWiki'
+
+describe('Give `DogWiki`', () => {
+
+    let component
+
+    function requiredProps(overrides = {}) {
+        return {
+            ...overrides
+        };
+    }
+
+    function renderComponent(props = requiredProps()) {
+        return shallow(<DogWiki {...props} />);
+    }
+
+    beforeEach(() => {
+        component = renderComponent();
+    })
+
+    it('should exist as a `section` with a specific class name', () => {
+
+        expect(component.find('.dog-show').type()).to.equal('section');
+
+    });
+
+});
