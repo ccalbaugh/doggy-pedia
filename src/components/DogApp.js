6bb701444ac1b3f2cb9a1eba788404214321e577
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 10:32:15 -0600

    refactor: change DogSearchForm to a class component to leverage the controlled input

--- src/components/DogApp.js
+++ src/components/DogApp.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import DogSearchForm from './DogSearchForm'
 import DogShow from './DogShow'
 import DogWiki from './DogWiki'
 
 
-function DogApp() {
-    return (
-        <main className="dog-app">
-            <DogSearchForm />
-            <DogShow />
-            <DogWiki />
-        </main>
-    );
+class DogApp extends Component {
+    render() {
+        return (
+            <main className="dog-app">
+                <DogSearchForm />
+                <DogShow />
+                <DogWiki />
+            </main>
+        );
+    }
 }
 
 export default DogApp;
--- src/components/DogSearchForm.js
+++ src/components/DogSearchForm.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function DogSearchForm() {
-    return (
-        <form className="dog-search-form">
-            <input type="text" className="dog-search-input" />
-            <button type="submit" className="dog-search-button">Search</button>
-        </form>
-    );
+class DogSearchForm extends Component {
+
+    state = {
+        currentInput: ''
+    };
+
+    render() {
+        return (
+            <form className="dog-search-form">
+                <input type="text" className="dog-search-input" />
+                <button type="submit" className="dog-search-button">Search</button>
+            </form>
+        );
+    }
 }
 
 export default DogSearchForm;
