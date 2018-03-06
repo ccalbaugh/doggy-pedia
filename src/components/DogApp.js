9c6b69bc6b5aa6460ba00f6cb06c33d50826852c
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 09:40:23 -0600

    feat: fulfill test case for DogShow

--- src/components/DogApp.js
+++ src/components/DogApp.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import DogSearchForm from './DogSearchForm'
+import DogShow from './DogShow'
+
 
 function DogApp() {
     return (
         <main className="DogApp">
             <DogSearchForm />
+            <DogShow />
         </main>
     );
 }
