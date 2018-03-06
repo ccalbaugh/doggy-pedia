e4840807e3b14d838713bf38875e44462dbf3fc4
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 10:33:10 -0600

    style: add basic css to project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DogApp from './components/DogApp'
+import './index.css'
 
 ReactDOM.render(<DogApp />, document.getElementById('root'));
