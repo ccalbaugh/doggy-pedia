664ec3c3420478ea76a56d61f5285aa6e9902b41
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 09:48:55 -0600

    feat: fulfill DogApp test

--- src/components/DogApp.js
+++ src/components/DogApp.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import DogSearchForm from './DogSearchForm'
 import DogShow from './DogShow'
+import DogWiki from './DogWiki'
 
 
 function DogApp() {
     return (
-        <main className="DogApp">
+        <main className="dog-app">
             <DogSearchForm />
             <DogShow />
+            <DogWiki />
         </main>
     );
 }
