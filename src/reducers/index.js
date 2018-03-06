f37e592fbf8da94b99b273bbe6c5312951f40d31
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 14:40:36 -0600

    feat: create fetchAllBreeds to fulfill corresponding test case

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,16 @@
 import * as types from '../actions/actionTypes'
 
+function fetchAllBreeds(state, action) {
+    return {
+        ...state,
+        allBreeds: action.breeds
+    };
+}
+
 export default function(state = {}, action) {
-    const actionsHandler = {};
+    const actionsHandler = {
+        [types.FETCH_ALL_BREEDS]: fetchAllBreeds
+    };
 
     const reducer = actionsHandler[action.type];
 
