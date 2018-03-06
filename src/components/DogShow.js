e5fdde039dd4e37b19c66e66171daefee94f1d5c
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-06 10:02:56 -0600

    feat: fulfill corresponding test

--- src/components/DogShow.js
+++ src/components/DogShow.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import DogGallery from './DogGallery'
 
 function DogShow() {
     return (
         <section className="dog-show">
+            <button className="previous-button">Prev</button>
+            <DogGallery />
+            <button className="next-button">Next</button>            
         </section>
     );
 }
