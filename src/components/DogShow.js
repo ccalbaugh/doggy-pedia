3003c87e62cb0d182f73105ffec76636858205b6
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-07 11:35:42 -0600

    feat: create logic to fulfill corresponding tests

--- src/components/DogShow.js
+++ src/components/DogShow.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { fetchAllBreeds } from '../actions';
-import DogGallery from './DogGallery'
+import DogGallery from './DogGallery';
+import BreedChoice from './BreedChoice';
 
 export class DogShow extends Component {
-
+    
     componentDidMount() {
         this.props.fetchAllBreeds();
     }
 
     render() {
-        console.log('Breeds List: ', this.props.breedsList);
+        const { currentBreed } = this.props;
         return (
             <section className="dog-show">
-                <button className="previous-button">Prev</button>
-                <DogGallery />
-                <button className="next-button">Next</button>            
+                {
+                    (!currentBreed || currentBreed.length === 0) ? (
+                        <span className="no-breeds-text">Search For a Breed</span>
+                    ) : (
+                        currentBreed.length === 1 ? (
+                            <div className='dog-gallery-container'>
+                                <button className="previous-button">Prev</button>
+                                <DogGallery currentBreed={currentBreed} />
+                                <button className="next-button">Next</button>   
+                            </div>
+                        ) : (
+                            <div className="breed-choice-container">
+                                {
+                                    currentBreed.map((breed, i) => {
+                                        return <BreedChoice key={i} />;
+                                    })
+                                }
+                            </div>
+                        )
+                    )
+                }     
             </section>
         );
     }
 }
 
+DogShow.propTypes = {
+    currentBreed: PropTypes.array,
+    fetchAllBreeds: PropTypes.func
+};
+
 function mapStateToProps(state) {
     return {
-        breedsList: state.allBreeds
+        currentBreed: state.currentBreed
     };
 }
 
