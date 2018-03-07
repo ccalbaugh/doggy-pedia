409128130812e34f768762f20cc4b9e63c3041e0
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-07 12:26:50 -0600

    test: ensure the root reducer handles fetchCurrentBreedImages

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -3,6 +3,11 @@ import * as types from '../actions/actionTypes'
 import dogReducer from './index'
 
 const mockBreeds = [ "affenpinscher", "african", "airedale", "akita" ];
+const mockImages = [
+    "https://dog.ceo/api/img/akita/512px-Ainu-Dog.jpg",
+    "https://dog.ceo/api/img/akita/Akita_Dog.jpg",
+    "https://dog.ceo/api/img/akita/Akita_Inu_dog.jpg"
+];
 
 describe('Given `dogReducer`', () => {
 
@@ -28,6 +33,16 @@ describe('Given `dogReducer`', () => {
 
         expect(dogReducer({ allBreeds: mockBreeds }, { type: types.SEARCH_BREED, breed: 'akita' })).to.equal(expectedState);
         
+    })
+
+    it('should handle `FETCH_CURRENT_BREED_IMAGES', () => {
+
+        const initialState = { allBreeds: mockBreeds, currentBreed: [ 'akita' ] };
+
+        const expectedState = { allBreeds: mockBreeds, currentBreed: [ 'akita' ], currentBreedImages: mockImages };        
+
+        expect(dogReducer(initialState, { type: types.FETCH_CURRENT_BREED_IMAGES, images: mockImages })).to.equal(expectedState);
+
     })
 
 });
