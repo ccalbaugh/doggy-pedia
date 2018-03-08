ecd9fd50d6147ea35e56b3e6179711f9dddc28f0
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-08 10:31:50 -0600

    style: format code for better readability

--- src/components/DogGallery.js
+++ src/components/DogGallery.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function DogGallery(props) {
-    console.log("DogGallery props: ", props);
     return (
         <ul className="dog-gallery">
             {
@@ -10,7 +9,16 @@ function DogGallery(props) {
                     <span className='no-breed-images-text'>No Images Found</span>
                 ) : (
                     props.breedImages.map(image => {
-                        return <li className="dog-gallery-item" key={`${image}-key`}><img src={image} alt="A Dog"/></li>
+                        return (
+                            <li className="dog-gallery-item" 
+                                key={`${image}-key`}
+                            >
+                                <img src={image} 
+                                    alt="A Dog"
+                                    className="breed-image"
+                                />
+                            </li>
+                        );
                     })
                 )   
             }
@@ -19,7 +27,7 @@ function DogGallery(props) {
 }
 
 DogGallery.propTypes = {
-    breedImages: PropTypes.array.isRequired
+    breedImages: PropTypes.array
 }
 
 export default DogGallery;
