64cbd41902ca280995db864a52dba67a9ae20531
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-09 12:15:30 -0600

    feat: add editing editor and preview ediitor per test spec

--- src/components/DogWiki.js
+++ src/components/DogWiki.js
@@ -3,23 +3,25 @@ import Editor from 'draft-js-plugins-editor';
 import { EditorState } from 'draft-js';
 import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
 import createEmojiPlugin from 'draft-js-emoji-plugin';
-import 'draft-js-static-toolbar-plugin/lib/plugin.css';
 import 'draft-js-emoji-plugin/lib/plugin.css';
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
 import './DogWiki.css';
-
-const toolbarPlugin = createToolbarPlugin();
-const { Toolbar } = toolbarPlugin;
 
 const emojiPlugin = createEmojiPlugin();
 const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
 
-const plugins = [
-    toolbarPlugin,
-    emojiPlugin
-];
+const toolbarPlugin = createToolbarPlugin();
+
+const { Toolbar } = toolbarPlugin;
+
+const plugins = [toolbarPlugin, emojiPlugin];
 
 function onChange(editorState) {
     this.setState({ editorState });
+}
+
+function focus() {
+    this.editor.focus();
 }
 
 class DogWiki extends Component {
@@ -31,15 +33,21 @@ class DogWiki extends Component {
     render() {
         return (
             <section className="dog-wiki">
-                <div className="editor" onClick={this.focus}>
+                <div className="editor editing-editor" onClick={focus.bind(this)}>
                     <Editor
                         editorState={this.state.editorState}
                         onChange={onChange.bind(this)}
                         plugins={plugins}
+                        ref={(element) => { this.editor = element }}
                     />
                     <Toolbar />
                     <EmojiSuggestions />
-                    <EmojiSelect />                
+                    <EmojiSelect />            
+                </div>
+                <div className="editor preview-editor">
+                    <Editor
+                        editorState={this.state.editorState}
+                    />
                 </div>
             </section>
         );
