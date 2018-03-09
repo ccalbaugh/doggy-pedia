4b372e3fa11d9d2ab2f9340d6eb9e71833bf8f74
ccalbaugh <cadenalbaugh@gmail.com>
2018-03-09 08:55:22 -0600

    feat: create logic to fulfill corresponding test spec

--- src/components/DogWiki.js
+++ src/components/DogWiki.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import { EditorState } from 'draft-js';
+import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
+import './DogWiki.css';
+
+const toolbarPlugin = createToolbarPlugin();
+const { Toolbar } = toolbarPlugin;
+
+function onChange(editorState) {
+    this.setState({ editorState });
+}
 
 class DogWiki extends Component {
 
@@ -10,7 +21,14 @@ class DogWiki extends Component {
     render() {
         return (
             <section className="dog-wiki">
-                <Editor />
+                <div className="editor" onClick={this.focus}>
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={onChange.bind(this)}
+                        plugins={[toolbarPlugin]}
+                    />
+                    <Toolbar />                
+                </div>
             </section>
         );
     }
